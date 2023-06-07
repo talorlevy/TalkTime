98b73e63cb8d82fd04bab1da29cc3cfc6248c1e3
Talor Levy <talorlv123@hotmail.com>
2023-06-07 11:41:18 -0700

    Added SignUpViewControllerExtension file, reformatted SignUpViewController and SignUpViewModel

--- TalkTime/View Model/Start up/SignUpViewModel.swift
+++ TalkTime/View Model/Start up/SignUpViewModel.swift
@@ -5,9 +5,8 @@
 //  Created by Talor Levy on 3/10/23.
 //
 
-import UIKit
 import FirebaseAuth
-
+import UIKit
 
 class SignUpViewModel {
         
@@ -21,57 +20,71 @@ class SignUpViewModel {
         self.vc = vc
     }
     
-    
     // MARK: - Sign Up (check username, create account, create user in database, sign out)
 
-    func signUp(provider: Constants.Provider, email: String, username: String, firstName: String,
-                lastName: String, phone: String, dateOfBirth: Date, gender: Constants.Gender,
-                password: String, completion: @escaping(Result<Bool, Error>) -> Void) {
-        firebaseDBManager.checkIfUsernameExists(username: username) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let exists):
-                print("Success checking if username exists at SignUpViewModel")
-                if exists {
-                    completion(.success(exists))
-                } else {
-                    self.firebaseAuthManager.createUser(email: email, password: password) { result in
-                        switch result {
-                        case .success(let user):
-                            print("Success creating authentication account at SignUpViewModel")
-                            guard let user = user else { return }
-                            let userModel = UserModel(uid: user.uid, provider: provider, email: email,
-                                                      username: username, firstName: firstName, lastName: lastName,
-                                                      phone: phone, dateOfBirth: dateOfBirth, gender: gender)
-                            let userDictionary = Formatting.userToDictionary(userModel: userModel)
-                            self.firebaseDBManager.createUser(uid: user.uid, userDictionary: userDictionary) { result in
-                                switch result {
-                                case .success():
-                                    print("Success creating user at SignUpViewModel")
-                                    self.firebaseAuthManager.signOutOfFirebase { result in
-                                        switch result {
-                                        case .success():
-                                            print("Success signing out of Firebase at SignUpViewModel")
-                                            completion(.success(exists))
-                                        case .failure(let error):
-                                            print("Error signing out of Firebase at SignUpViewModel: \(error.localizedDescription)")
+    func signUp(provider: Constants.Provider,
+                email: String,
+                username: String,
+                firstName: String,
+                lastName: String,
+                phone: String,
+                dateOfBirth: Date,
+                gender: Constants.Gender,
+                password: String,
+                completion: @escaping(Result<Bool, Error>) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            self?.firebaseDBManager.checkIfUsernameExists(username: username) { result in
+                switch result {
+                case .success(let exists):
+                    if exists {
+                        completion(.success(exists))
+                    } else {
+                        self?.firebaseAuthManager.createUser(email: email, password: password) { result in
+                            switch result {
+                            case .success(let user):
+                                guard let user = user else { return }
+                                let userModel = UserModel(uid: user.uid,
+                                                          provider: provider,
+                                                          email: email,
+                                                          username: username,
+                                                          firstName: firstName,
+                                                          lastName: lastName,
+                                                          phone: phone,
+                                                          dateOfBirth: dateOfBirth,
+                                                          gender: gender)
+                                let userDictionary = Formatting.userToDictionary(userModel: userModel)
+                                self?.firebaseDBManager.createUser(uid: user.uid,
+                                                                  userDictionary: userDictionary) { result in
+                                    switch result {
+                                    case .success():
+                                        self?.firebaseAuthManager.signOutOfFirebase { result in
+                                            DispatchQueue.main.async {
+                                                switch result {
+                                                case .success():
+                                                    completion(.success(exists))
+                                                case .failure(let error):
+                                                    completion(.failure(error))
+                                                }
+                                            }
+                                        }
+                                    case .failure(let error):
+                                        DispatchQueue.main.async {
                                             completion(.failure(error))
                                         }
                                     }
-                                case .failure(let error):
-                                    print("Error creating user at SignUpViewModel")
+                                }
+                            case .failure(let error):
+                                DispatchQueue.main.async {
                                     completion(.failure(error))
                                 }
                             }
-                        case .failure(let error):
-                            print("Error creating authentication account at SignUpViewModel")
-                            completion(.failure(error))
                         }
                     }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
                 }
-            case .failure(let error):
-                print("Error checking if username exists at SignUpViewModel")
-                completion(.failure(error))
             }
         }
     }
